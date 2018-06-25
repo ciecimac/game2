2af24fa0ae28fc6681017a3fa6d738c8789c1f62
ciecimac <ciecclm@imnu.edu.cn>
2018-06-25 17:40:09 +0800

    更改了形参变量名
    将Hurted(int nPower)里的年nPower形参名改成了nPow,避免同学们把函数形参与类成员变量名搞混。

--- game2/game2.cpp
+++ game2/game2.cpp
@@ -25,11 +25,11 @@ public:
 		nLifeValue = l;
 		name = name_;
 	}
-	void Hurted(int nPower)
+	void Hurted(int nPow)
 	{
 		//．．．．表现受伤动作的代码
 		cout << name << "受伤害" << endl;
-		nLifeValue -= nPower;
+		nLifeValue -= nPow;
 	}
 	void Attack(CCreature * pobj)
 	{
